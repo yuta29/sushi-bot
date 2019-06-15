79d6e12ade999c382a4c99e766877175f6700a7a
yuta29 <47266960+yuta29@users.noreply.github.com>
2019-06-15 15:21:44 +0900

    Improve

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
+            if (event.message.text == "hello"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "これはこれは"
+                    text: "ohoho"
                 }));
             }
         }
